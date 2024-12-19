dc3f9d8e0f083a4e1a0243d8a8285655d45ded1a
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-12-19 23:00:33 +0100

    Grund

---
+++ strings.lua
@@ -0,0 +1,78 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Bombardier Traxx 1",
+		["mod_desc"] = "The Vectron is a modern three-phase locomotive for international passenger and freight transport in Europe. The Vectron can operate in 20 European countries. The Vectron was first presented to the public in 2010, and since then the locomotive has been in use on many state and private railways. The package contains the locomotives class 193 MS (multi-system), class 193 AC (alternating current) and the Smartron.",
+
+
+	},
+	de = {
+
+		["mod_name"] = "Traxx 1 Repaintpaket SBB CFF FFS Cargo ",
+		["mod_desc"] = "Dies ist das SBB CFF FFS Cargo Repaintpaket für die Traxx F140 AC von der ModWerkstatt. In diesem Paket sind weitere 17 Lackierungen enthalten. Dieses Paket entstande Kooperation und mit Erlaubnis der SBB CFF FFS Cargo.",
+				
+		["re482_sbb_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb2_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb3_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb4_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb5_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb6_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb7_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb8_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb9_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb10_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb11_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb12_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb13_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb14_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb15_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb16_name"] = "Re482 000 SBB Cargo (2000)",
+		["re482_sbb17_name"] = "Re482 000 SBB Cargo (2000)",
+						
+		 -- Vorspann
+		
+		["re482_sbb_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb2_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb3_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb4_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb5_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb6_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb7_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb8_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb9_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb10_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb11_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb12_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb13_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb14_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb15_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb16_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		["re482_sbb17_name_vorspann"] = "Vorspann - Re482 000 SBB Cargo (2000)",
+		
+		
+        -- Fake
+		
+		["re482_sbb_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb2_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb3_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb4_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb5_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb6_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb7_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb8_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb9_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb10_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb11_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb12_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb13_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb14_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb15_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb16_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		["re482_sbb17_name_fake"] = "Fake - Re482 000 SBB Cargo (2000)",
+		
+
+
+	},
+}
+end
