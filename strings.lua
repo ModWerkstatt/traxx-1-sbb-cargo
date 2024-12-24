f30782e3fb893d01e03dde46fa4bd42e28f4d7f1
Florian Thiel <flth@flth.de>
2024-12-24 23:51:34 +0100

    Strings bearbeitet

--- strings.lua
+++ strings.lua
@@ -3,7 +3,7 @@ return {
 	en = {
 
 		["mod_name"] = "Traxx 1 SBB Cargo repaint package",
-		["mod_desc"] = "This is a repaint package from SBB CFF FFS Cargo and SBB Cargo International for the Traxx F140 AC1 from ModWerkstatt. An additional 13 paint finishes are included in this package. This package was created in cooperation and with the permission of SBB CFF FFS Cargo and SBB Cargo Internaitonal.",
+		["mod_desc"] = "This is a repaint package of the Traxx 1 F140 AC1 from the ModWerkstatt with repaints of the SBB CFF FFS Cargo and SBB Cargo International. This package contains 13 more repaints. This package was created in co-operation and with the permission of SBB CFF FFS Cargo and SBB Cargo International.",
 
 		["br185_desc"] = "The Traxx AC1 F140 is a dual-system electric freight locomotive that can be delivered with 2 or 4 pantographs. It can be equipped with various country packages, e.g. for Switzerland or France. Most Deutsche Bahn locomotives were only supplied with 2 pantographs for use in Germany and Austria.",
 
@@ -28,21 +28,21 @@ return {
 
 		 -- Vorspann
 
-		["re482_sbb_name_vorspann"] = "Vorspann - Re 482 000 SBB Cargo - Cologne(2003)",
-		["re482_sbb2_name_vorspann"] = "Vorspann - Re 482 011 SBB Cargo - Train driver (2007)",
-		["re482_sbb3_name_vorspann"] = "Vorspann - Re 482 025 SBB Cargo (2009)",
-		["re482_sbb4_name_vorspann"] = "Vorspann - Re 482 010 SBB Cargo - Application SBBCINT (2012)",
-		["re482_sbb5_name_vorspann"] = "Vorspann - Re 482 008 SBB Cargo - Ökotrans (2005)",
-		["re482_sbb6_name_vorspann"] = "Vorspann - Re 482 023 SBB Cargo - Olten (2011)",
-		["re482_sbb7_name_vorspann"] = "Vorspann - Re 482 008 SBB Cargo - Ökotrans (2015)",
-		["re482_sbb8_name_vorspann"] = "Vorspann - Re 482 009 SBB Cargo - small logo (2006)",
-		["re482_sbb9_name_vorspann"] = "Vorspann - Re 482 009 SBB Cargo - Strong machine (2015)",
-		["re482_sbb10_name_vorspann"] = "Vorspann - Re 482 026 SBB Cargo - Alpäzähmer (2015)",
-		["re482_sbb11_name_vorspann"] = "Vorspann - Re 482 013 SBB Cargo - Alpäzähmer (2016)",
-		["re482_sbb12_name_vorspann"] = "Vorspann - Re 482 023 SBB Cargo - SBBCINT(2015)",
-		["re482_sbb13_name_vorspann"] = "Vorspann - Re 482 001 SBB Cargo - Traffic red (2020)",
-		["re482_sbb14_name_vorspann"] = "Vorspann - Re 482 020 SBB Cargo - Europe (2024)",
-		["re482_sbb15_name_vorspann"] = "Vorspann - Re 482 012 SBB Cargo - Maintenance with a clear view (2024)",
+		["re482_sbb_name_vorspann"] = "Lead - Re 482 000 SBB Cargo - Cologne(2003)",
+		["re482_sbb2_name_vorspann"] = "Lead - Re 482 011 SBB Cargo - Train driver (2007)",
+		["re482_sbb3_name_vorspann"] = "Lead - Re 482 025 SBB Cargo (2009)",
+		["re482_sbb4_name_vorspann"] = "Lead - Re 482 010 SBB Cargo - Application SBBCINT (2012)",
+		["re482_sbb5_name_vorspann"] = "Lead - Re 482 008 SBB Cargo - Ökotrans (2005)",
+		["re482_sbb6_name_vorspann"] = "Lead - Re 482 023 SBB Cargo - Olten (2011)",
+		["re482_sbb7_name_vorspann"] = "Lead - Re 482 008 SBB Cargo - Ökotrans (2015)",
+		["re482_sbb8_name_vorspann"] = "Lead - Re 482 009 SBB Cargo - small logo (2006)",
+		["re482_sbb9_name_vorspann"] = "Lead - Re 482 009 SBB Cargo - Strong machine (2015)",
+		["re482_sbb10_name_vorspann"] = "Lead - Re 482 026 SBB Cargo - Alpäzähmer (2015)",
+		["re482_sbb11_name_vorspann"] = "Lead - Re 482 013 SBB Cargo - Alpäzähmer (2016)",
+		["re482_sbb12_name_vorspann"] = "Lead - Re 482 023 SBB Cargo - SBBCINT(2015)",
+		["re482_sbb13_name_vorspann"] = "Lead - Re 482 001 SBB Cargo - Traffic red (2020)",
+		["re482_sbb14_name_vorspann"] = "Lead - Re 482 020 SBB Cargo - Europe (2024)",
+		["re482_sbb15_name_vorspann"] = "Lead - Re 482 012 SBB Cargo - Maintenance with a clear view (2024)",
 
         -- Fake
 
@@ -66,8 +66,8 @@ return {
 	de = {
 
 		["mod_name"] = "Traxx 1 SBB Cargo Repaintpaket",
-		["mod_desc"] = "Dies ist Repaintpaket der SBB CFF FFS Cargo sowie der SBB Cargo International für die Traxx F140 AC1 von der ModWerkstatt. In diesem Paket sind weitere 13 Lackierungen enthalten. Dieses Paket entstande Kooperation und mit Erlaubnis der SBB CFF FFS Cargo und der SBB Cargo Internaitonal.",
-
+		["mod_desc"] = "Dies ist Repaintpaket von der Traxx 1 F140 AC1 von der ModWerkstatt mit Repaints der SBB CFF FFS Cargo sowie der SBB Cargo International. In diesem Paket sind weitere 13 Lackierungen enthalten. Dieses Paket entstand in Kooperation und mit Erlaubnis der SBB CFF FFS Cargo und der SBB Cargo International.",
+i
 		["br185_desc"] = "Die Traxx AC1 F140 ist eine zweisystemfähige elektrische Güterlokomotive, die mit 2 oder 4 Stromabnehmern ausgeliefert werden kann. Sie kann mit verschiedenen Länderpaketen ausgerüstet werden z.B. für die Schweiz oder Frankreich. Die meisten Loks der Deutschen Bahn wurden nur mit 2 Pantographen geliefert für den Einsatz in Deutschland und Österreich.",
 
 		["br185_ch"] = 	" BR 185 mit 4 Panto für Schweiz, Frankreich oder Luxemburg",
